cc73384e93eea8ec69d6cd189a10bd7c5ac649bd
quan <quan_xiaoxiao@qq.com>
2025-08-11 11:04:05 +0800

    xxx

--- src/getSocketConnect.mjs
+++ src/getSocketConnect.mjs
@@ -2,37 +2,68 @@ import assert from 'node:assert';
 import net from 'node:net';
 import tls from 'node:tls';
 
+const PROTOCOLS = {
+  HTTP: 'http:',
+  HTTPS: 'https:',
+};
+
+const DEFAULT_PORTS = {
+  [PROTOCOLS.HTTP]: 80,
+  [PROTOCOLS.HTTPS]: 443,
+};
+
+const BUFFER_SIZES = {
+  HTTP: 64 * 1024,
+  HTTPS: 16384,
+};
+
+const validateInputs = ({ port, protocol }) => {
+  if (port != null) {
+    assert(
+      Number.isInteger(port) && port >= 0 && port <= 65535,
+      'port must be an integer between 0 and 65535',
+    );
+  }
+
+  assert(
+    Object.values(PROTOCOLS).includes(protocol),
+    `protocol must be either '${PROTOCOLS.HTTP}' or '${PROTOCOLS.HTTPS}'`,
+  );
+};
+
+const createTlsConnection = (commonOptions, { servername, rejectUnauthorized }) => {
+  const tlsOptions = {
+    ALPNProtocols: ['http/1.1'],
+    rejectUnauthorized,
+    secureContext: tls.createSecureContext({
+      secureProtocol: 'TLSv1_2_method',
+    }),
+    ...(servername && { servername }),
+  };
+
+  return tls.connect({ ...commonOptions, ...tlsOptions });
+};
+
 export default ({
   hostname,
   port,
-  protocol = 'http:',
+  protocol = PROTOCOLS.HTTP,
   servername,
   rejectUnauthorized = true,
 }) => {
-  if (port != null) {
-    assert(port >= 0 && port <= 65535 && Number.isInteger(port));
-  }
-  const isHttps = protocol === 'https:';
-  const defaultPort = isHttps ? 443 : 80;
+  validateInputs({ port, protocol });
+  const isHttps = protocol === PROTOCOLS.HTTPS;
+  const targetPort = port ?? DEFAULT_PORTS[protocol];
 
   const commonOptions = {
     host: hostname,
-    port: port ?? defaultPort,
+    port: targetPort,
     noDelay: true,
-    highWaterMark: isHttps ? 16384 : 64 * 1024,
+    highWaterMark: isHttps ? BUFFER_SIZES.HTTPS : BUFFER_SIZES.HTTP,
   };
 
   if (isHttps) {
-    const tlsOptions = {
-      ALPNProtocols: ['http/1.1'],
-      rejectUnauthorized,
-      secureContext: tls.createSecureContext({
-        secureProtocol: 'TLSv1_2_method',
-      }),
-      ...(servername && { servername }),
-    };
-
-    return tls.connect(Object.assign(commonOptions, tlsOptions));
+    return createTlsConnection(commonOptions, { servername, rejectUnauthorized });
   }
 
   assert(protocol === 'http:', 'Protocol must be either `http:` or `https:`');
